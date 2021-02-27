789c31125ccf1af5939a8498c72332572d805225
Jorge Peris <jorgeperis@gmail.com>
2021-02-27 08:50:56 +0100

    Add validations for Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  validates :first_name, presence: true, length: { maximum: 255 }
+  validates :last_name, presence: true, length: { maximum: 255 }
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :phone_number,
+            presence: true,
+            length: { minimum: 9, maximum: 255 }
 end
